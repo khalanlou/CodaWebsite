8e7bf9e276760fdb673637d82b35d0805f077837
Khalanlou <Khalanlou@gmail.com>
2020-06-24 19:13:31 +0430

    header and nav updated

---
+++ LandingPage/Landing.js
@@ -0,0 +1,8 @@
+function sumbitbutton() {
+    var element = document.getElementById("Navbar_Left_Contact_Sign_Button_Icon");
+    element.classList.add("spinner-grow text-light");
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
