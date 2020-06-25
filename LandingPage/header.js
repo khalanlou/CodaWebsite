e0ef661c7f5ad92b313e5f5cb8474e3fdb7f8dc8
Khalanlou <Khalanlou@gmail.com>
2020-06-25 13:09:03 +0430

    header and navbar completed!

--- LandingPage/header.js
+++ LandingPage/header.js
@@ -3,9 +3,11 @@ function openNav() {
     document.getElementById("side-nav").style.width = "350px";
     document.getElementById("Main").style.marginLeft = "50px";
     document.getElementById("side-nav").style.backgroundColor = "rgb(255, 233, 233, 0.5)";
+    document.getElementById("header").style.opacity = "80%";
   }
   
   function closeNav() {
+    document.getElementById("header").style.opacity = "100%";
     document.getElementById("side-nav").style.width = "0";
     document.getElementById("Main").style.marginLeft= "0";
   }
