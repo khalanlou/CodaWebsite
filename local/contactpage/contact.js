9b7098c6eb1fa81ec081466d41fb5f4dd0ec960c
Khalanlou <Khalanlou@gmail.com>
2020-07-08 15:00:16 +0430

    fixing width in large scremens configured

--- local/contactpage/contact.js
+++ local/contactpage/contact.js
@@ -48,3 +48,19 @@ function scrollFunction() {
   }
 }
 /**************** navbar-scroll ****************/
+
+/**************** fix-width ****************/
+function resize() {
+  if ( $(window).width() > 1370) {     
+    $("#main").addClass('container');
+    $("#main").removeClass('container-fluid');
+  }
+  else
+  {
+    $("#main").addClass('container-fluid');
+    $("#main").removeClass('container');
+  }
+}
+$(window).on("resize", resize);
+resize();
+/**************** fix-width ****************/
